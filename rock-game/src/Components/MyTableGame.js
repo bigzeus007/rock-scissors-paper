6083fc18a4232fe52a93c43b6c161043dc5dce27
said.T <tarhisaid@gmail.com>
2021-07-05 21:07:46 +0100

    Rock Paper sissors game React
    UseEffect()

--- rock-game/src/Components/MyTableGame.js
+++ rock-game/src/Components/MyTableGame.js
@@ -4,31 +4,32 @@ import { useState,useEffect } from 'react';
 
 function MyTableGame() {
     const numPartie=0
-   
+    const [monChoix,setMonChoix]=useState("Hello World")
     const images=["rockRobot","scissorsRobot","paperRobot"]
+   const myRandomImage =images[Math.floor(Math.random()*images.length)]
+   const[sonChoix,setSonChoix]=useState("hey")
    
-    const[sonChoix,setSonChoix]=useState("hey")
-    const [monChoix,setMonChoix]=useState({monChoix:"monChoix",sonChoix:"sonChoix"})
+   
+  
 
     function setJeu(element) {
-        
-        if(numPartie<=4){
-           const robotChoix =()=>{return document.getElementById("sonChoix").className}
-            if (element===robotChoix()) {console.log("egalite  "+element+' '+robotChoix())}else{console.log("pas Egalite  "+element+' '+robotChoix())
-           
-    };
+    }
 
+    useEffect(()=>{if (monChoix[0]===sonChoix[0]) 
+        {console.log("egalite  "+monChoix+' '+sonChoix)
+    }else{
+        console.log("pas Egalite  "+" son choix "+sonChoix+" Mon choix "+monChoix)}
         
-    }}
+    })
 
     return(
         <div id="monJeu">
         <div id="maListeDeChoix">
     
-        <button className="rock" onClick={(e)=>{setMonChoix(monChoix:"rock",sonChoix:{images[Math.floor(Math.random()*images.length)]});setJeu(e.target.className);}}/>
-        <button onClick={(e)=>{setMonChoix(monChoix:"scissors",sonChoix:{images[Math.floor(Math.random()*images.length)]});setJeu(e.target.className);}}  className="scissors"/>
-        <button onClick={(e)=>{setMonChoix(monChoix:"paper",sonChoix:{images[Math.floor(Math.random()*images.length)]});setJeu(e.target.className);}}  className="paper"/>
-        <div id="monChoix" className={monChoix.monChoix} ></div>
+        <button className="rock" onClick={(e)=>{setMonChoix("rock");setSonChoix(myRandomImage)}} />
+        <button className="scissors" onClick={(e)=>{ setMonChoix("scissors");setSonChoix(images[Math.floor(Math.random()*images.length)]);}} />
+        <button className="paper" onClick={(e)=>{setMonChoix("paper");setSonChoix(images[Math.floor(Math.random()*images.length)]);}} />
+        <div id="monChoix"  className={monChoix} />
         </div>
 
 
@@ -37,7 +38,7 @@ function MyTableGame() {
         <button className="rockRobot"/>
         <button className="scissorsRobot"/>
         <button className="paperRobot"/>
-        <button  id="sonChoix" className={monChoix.sonChoix}/>
+        <button  id="sonChoix" className={sonChoix}/>
         
         </div>
         </div>

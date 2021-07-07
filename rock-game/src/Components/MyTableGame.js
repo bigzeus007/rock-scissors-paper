c5eb9d3cb5162473735642debc3f3227538b798b
said.T <tarhisaid@gmail.com>
2021-07-07 22:49:04 +0100

    paper sissors rock React game
    fin exercice

--- rock-game/src/Components/MyTableGame.js
+++ rock-game/src/Components/MyTableGame.js
@@ -6,9 +6,11 @@ function MyTableGame() {
     const [numPartie,setNumPartie]=useState(0)
     const [monScore,setMonScore]=useState(0)
     const [sonScore,setSonScore]=useState(0)
+    const [monScoreF,setMonScoreF]=useState(0)
+    const [sonScoreF,setSonScoreF]=useState(0)
     const [monChoix,setMonChoix]=useState("Hello World")
-    const [myAvatar,setMyAvatar]=useState("")
-    const [hisAvatar,setHisAvatar]=useState("")
+    const [myAvatar,setMyAvatar]=useState("Start")
+    const [hisAvatar,setHisAvatar]=useState("Start2")
     const images=["rockRobot","scissorsRobot","paperRobot"]
    const myRandomImage =images[Math.floor(Math.random()*images.length)]
    const[sonChoix,setSonChoix]=useState("hey")
@@ -19,10 +21,7 @@ function MyTableGame() {
     function setJeu(element) {
     }
 
-    useEffect(()=>{if(monScore>=5){}
-        
-        
-        if (monChoix[0]===sonChoix[0]){
+    useEffect(()=>{if (monChoix[0]===sonChoix[0]){
         setMyAvatar("myEgalite")
         setHisAvatar("hisEgalite")
         console.log("EGALITE  ")
@@ -63,9 +62,24 @@ function MyTableGame() {
         setHisAvatar("youLoose")
         console.log("Vous avez gagne, This sissors gagne")
 }
-        
     },[numPartie])
-
+    useEffect(()=>{
+    
+        if(monScore>=5){
+            setMyAvatar("iAmBigWinner")
+            setHisAvatar("youBigLoose")
+            setSonScore(0)
+            setMonScore(0)
+            setMonScoreF(monScoreF+1)
+        }
+        if(sonScore>=5){
+            setMyAvatar("iBigLoose")
+            setHisAvatar("urBigWinner")
+            setSonScore(0)
+            setMonScore(0)
+            setSonScoreF(sonScoreF+1)
+        }
+        })
     return(
         <div id="monJeu">
             <div id="saListeDeChoix">
@@ -75,11 +89,15 @@ function MyTableGame() {
         </div>
             
             <div id="myResult" className={""}>
-                <span className={hisAvatar} style={{backgroundColor:"teal"}}></span>
-                <span className={myAvatar} style={{backgroundColor:"blue"}}></span>
+            <div id="sonScoreF" className="sonScoreF" style={{position:"relative",left:"150px",height:"50px",width:"400px",backgroundColor:"lime",fontSize:"30px",}} children={"La Machine a gagne "+sonScoreF+" Match"}/>
+            <div id="monScoreF" className="monScoreF" style={{position:"relative",left:"150px",height:"50px",width:"400px",backgroundColor:"lime",fontSize:"30px",}} children={"J ai gagne "+monScoreF+" Match"}/>
+            <div id="sonScore" className="sonScore" style={{position:"relative",left:"200px",height:"50px",width:"300px",fontSize:"30px",}} children={"Score Machine "+sonScore}/>
+            <div id="monScore" className="monScore" style={{position:"relative",left:"200px",height:"50px",width:"300px",fontSize:"30px",}} children={"Mon Score "+monScore}/>
+            <span className={hisAvatar} style={{backgroundColor:"teal"}}></span>
+            <span className={myAvatar} style={{backgroundColor:"blue"}}></span>
             <button  id="sonChoix" className={sonChoix}/>
             <button id="monChoix"  className={monChoix} />  
-            <div id="Score" className="Score" style={{height:"200px",backgroundColor:"yellow",fontSize:"70px",}} children={"Score Machine"+sonScore+"  "+"mon Score"+monScore}/>
+            
               
             </div>
             <div id="maListeDeChoix">
